d4f1579e6a1ba4383af127a5ea940fd0b7842df4
michalpazur <michal.pazur1@gmail.com>
2021-10-21 22:16:09 +0200

    Added menu for mobile screens to toolbar

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,15 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import MuiToolbar from "@material-ui/core/Toolbar";
 import Avatar from "@material-ui/core/Avatar";
 import Typography from "@material-ui/core/Typography";
 import MuiLink from "@material-ui/core/Link";
+import Hidden from "@material-ui/core/Hidden";
+import Collapse from "@material-ui/core/Collapse";
 import Button from "@material-ui/core/Button";
+import Zoom from "@material-ui/core/Zoom";
+import MenuIcon from "@material-ui/icons/MenuRounded";
+import CloseIcon from "@material-ui/icons/CloseRounded";
 import LightThemeIcon from "@material-ui/icons/WbSunnyOutlined";
 import DarkThemeIcon from "@material-ui/icons/Brightness2Outlined";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import { Link, useLocation } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { useSelector, useDispatch } from "react-redux";
 import { themeAction } from "../../redux/modules/theme";
 import clsx from "clsx";
@@ -19,6 +25,10 @@ const links = [
   {
     name: "Flag list",
     link: "/",
+  },
+  {
+    name: "FAQ",
+    link: "/faq",
   },
 ];
 
@@ -36,15 +46,15 @@ const useStyles = makeStyles((theme) => ({
   },
   avatar: {
     marginRight: "20px",
-    [theme.breakpoints.down("xs")]: {
-      display: "none",
-    },
   },
   name: {
     fontWeight: "bold",
     fontSize: "18px",
     color: theme.palette.text.primary,
     marginRight: "20px",
+    [theme.breakpoints.down("xs")]: {
+      display: "none",
+    },
   },
   spacer: {
     flexGrow: "1",
@@ -55,6 +65,14 @@ const useStyles = makeStyles((theme) => ({
     fontSize: "18px",
     "&:not(:last-of-type)": {
       marginRight: "20px",
+      [theme.breakpoints.down("sm")]: {
+        marginRight: "0px",
+      },
+    },
+    [theme.breakpoints.down("sm")]: {
+      padding: "10px 0px",
+      width: "100%",
+      textAlign: "center",
     },
   },
   activeLink: {
@@ -66,40 +84,57 @@ const useStyles = makeStyles((theme) => ({
   twitter: {
     color: theme.palette.primary.main,
   },
+  iconSpacer: {
+    width: "24px",
+    height: "24px",
+  },
+  icon: {
+    position: "absolute",
+  },
+  bottomRoot: {
+    paddingBottom: "20px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
 }));
 
 const Toolbar = () => {
   const classes = useStyles();
+  const [isOpen, setIsOpen] = useState(false);
   const theme = useSelector((state) => state.themeReducer.theme);
+  const muiTheme = useTheme();
+  const isMobile = useMediaQuery(muiTheme.breakpoints.down("sm"));
   const dispatch = useDispatch();
   const location = useLocation();
 
   useEffect(() => {
-    console.log(location.pathname);
-  }, [location]);
+    if (!isMobile) {
+      setIsOpen(false);
+    }
+  }, [isMobile]);
 
-  return (
-    <AppBar elevation={3} className={classes.appbar} color="transparent">
-      <MuiToolbar className={classes.toolbar}>
-        <Avatar src={icon} className={classes.avatar} />
-        <Typography className={classes.name}>PrideFlagBot</Typography>
-        <div className={classes.spacer}>
-          {links.map((link) => (
-            <MuiLink
-              color="textPrimary"
-              component={Link}
-              underline="none"
-              to={link.link}
-              aria-selected={location.pathname === link.link}
-              className={clsx(classes.link, {
-                [classes.activeLink]: location.pathname === link.link,
-              })}
-              key={link.link}
-            >
-              {link.name}
-            </MuiLink>
-          ))}
-        </div>
+  const renderLinks = () => {
+    return links.map((link) => (
+      <MuiLink
+        color="textPrimary"
+        component={Link}
+        underline="none"
+        to={link.link}
+        aria-selected={location.pathname === link.link}
+        className={clsx(classes.link, {
+          [classes.activeLink]: location.pathname === link.link,
+        })}
+        key={link.link}
+      >
+        {link.name}
+      </MuiLink>
+    ));
+  };
+
+  const renderButtons = () => {
+    return (
+      <React.Fragment>
         <Button
           size="small"
           variant="outlined"
@@ -125,7 +160,41 @@ const Toolbar = () => {
         >
           <TwitterIcon className={classes.twitter} />
         </Button>
+      </React.Fragment>
+    );
+  };
+
+  return (
+    <AppBar elevation={3} className={classes.appbar} color="transparent">
+      <MuiToolbar className={classes.toolbar}>
+        <Avatar src={icon} className={classes.avatar} />
+        <Typography className={classes.name}>PrideFlagBot</Typography>
+        <div className={classes.spacer}>
+          <Hidden smDown>{renderLinks()}</Hidden>
+        </div>
+        <Hidden smDown>{renderButtons()}</Hidden>
+        <Hidden mdUp>
+          <Button
+            size="small"
+            variant="outlined"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <div className={classes.iconSpacer} />
+            <Zoom in={isOpen}>
+              <CloseIcon className={classes.icon} />
+            </Zoom>
+            <Zoom in={!isOpen}>
+              <MenuIcon className={classes.icon} />
+            </Zoom>
+          </Button>
+        </Hidden>
       </MuiToolbar>
+      <Collapse in={isOpen}>
+        <div className={classes.bottomRoot}>
+          {renderLinks()}
+          <div>{renderButtons()}</div>
+        </div>
+      </Collapse>
     </AppBar>
   );
 };
